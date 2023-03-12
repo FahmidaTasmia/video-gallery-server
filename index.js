4b053dc8595d16f3c9417bfddeafe400a4ff28f9
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-03-12 13:59:41 +0600

    tags route added

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express ();
+require('dotenv').config();
+
+app.use (cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bkf4wz6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try {
+
+        const videoCollections = client.db('videoGallery').collection('videoCollection');
+        const tagCollections = client.db('videoGallery').collection('tags');
+
+        app.get('/videos', async(req,res)=>{
+            const query={};
+            const cursor = videoCollections.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+        app.get('/videos/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)};
+            const services = await videoCollections.findOne(query);
+            res.send(services);
+
+        });
+
+        app.get('/tags', async(req,res)=>{
+            const query={};
+            const cursor = tagCollections.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+    }
+
+    finally{
+
+    }
+}
+  
+run().catch(console.dir);
+
+app.get('/',async(req,res)=>{
+    res.send ('Video gallery Server is Running')
+})
+
+app.listen(port, ()=>console.log(`Video gallery is Running on ${port}`))
